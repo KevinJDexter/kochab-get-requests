13d165a27f0d7b6a1236cc0c189e3585c661fd57
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-18 15:14:09 -0500

    First AJAX usage

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,12 @@ $(document).ready(onReady);
 
 function onReady () {
   console.log('jQuery is loaded');
+
+  $.ajax({
+    type: 'GET',
+    url: '/all-quotes'
+  })
+  .then(function(response) {
+    console.log(response);
+  });
 }
