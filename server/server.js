8e72c5aac0a156148e1aec4a5bbfb583ab048a73
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-18 14:16:39 -0500

    A great deal of server learning

--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@ const express = require('express');
 
 const app = express();
 const PORT = 5000;
+const quotes_data = require('./all-quotes');
 
-// app.use(express.static('server/public'));
-app.get('/', function (req, res) {
-  res.send('Happy Wednesday');
+//   app.get('/', (req, res) => {
+// res.send('Happy Wednesday');
+// });
+
+app.use(express.static('server/public'));
+
+app.get('/all-quotes', (req, res) => {
+  res.send(quotes_data);
 });
 
-app.listen(PORT, function () {
+app.get('/quote', (req, res) => {
+  let randomQuote = quotes_data[Math.floor(Math.random() * quotes_data.length)];
+  res.send(randomQuote);
+})
+
+app.listen(PORT, () => {
   console.log('WOO BOI!!!');
 });
